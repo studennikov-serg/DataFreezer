a4d6b112997f44ba582097e1cada196ea965ca93
studennikov-serg <48173157+studennikov-serg@users.noreply.github.com>
2024-01-10 00:38:48 +0400

    XML output

--- Program.cs
+++ Program.cs
@@ -5,24 +5,23 @@ using System.Text;
 var rand = new Random();
 var fileContent = System.IO.File.ReadAllBytes(args[0]);
 var contentLength = fileContent.Length;
-//contentLength = 1025;
 Console.WriteLine($"content length: {contentLength}");
 var log2 = Math.Log2(contentLength);
 Console.WriteLine($"log2: {log2}");
-var pow = Math.Pow(2, log2);
-Console.WriteLine($"pow {pow}");
 
 var truncatedLog2 = (int)(log2);
-Console.WriteLine($"truncate log2: {truncatedLog2}");
-var maxBlockSize = 1 << truncatedLog2;
+Console.WriteLine($"truncated log2: {truncatedLog2}");
+var pow = Math.Pow(2, truncatedLog2);
+Console.WriteLine($"pow {pow}");
+var maxBlockSize = 1 << truncatedLog2; // less than file size and is a power of 2
 
 var fileHashes = ComputeHashes(maxBlockSize, fileContent, rand);
 Console.WriteLine($"[{fileHashes}]");
-//System.IO.File.WriteAllText(".", "out.hashes");
 
 static string ComputeHashes(int maxBlockSize, byte[] fileContent, Random rand)
 {
-    StringBuilder hashes = new StringBuilder();
+    var hashes = new StringBuilder();
+    hashes.AppendLine("<hashes>");
     MemoryStream stream = new System.IO.MemoryStream();
     for (int i = 0; i < 10; ++i)
     {
@@ -32,14 +31,11 @@ static string ComputeHashes(int maxBlockSize, byte[] fileContent, Random rand)
         var offset = rand.Next(fileContent.Length - blockSize);
         stream.Position = offset;
         stream.Read(blockData, 0, blockSize);
-        var hash = System.Security.Cryptography.SHA256.HashData(blockData);
-        hashes.
-        Append('"').Append(i).Append("\": {")
-        .Append("\"offset\": \"").Append(offset).Append("\", ").AppendLine()
-        .Append("\"blockSize\": \"").Append(blockSize).Append("\",").AppendLine()
-        .Append("\"hash\": \"").Append(Convert.ToHexString(hash)).Append("\" ]").AppendLine();
-        //.AppendLine(" }]");
+        var hash = Convert.ToHexString(System.Security.Cryptography.SHA256.HashData(blockData));
+        var dataEntity = $"<hash offset=\"{offset}\", blockSize=\"{blockSize}\">{hash}</hash>";
+        hashes.AppendLine(dataEntity);
     }
+    hashes.AppendLine("</hashes>");
     return hashes.ToString();
 }
 
